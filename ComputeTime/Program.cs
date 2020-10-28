d7de1a68ab676be5dd0574fc0f3b66c415818ed4
hieunguyentrong1996 <73066578+hieunguyentrong1996@users.noreply.github.com>
2020-10-28 21:31:09 +0700

    Add project files.

---
+++ ComputeTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var span = ComputeTime(DateTime.Now, DateTime.Now.AddDays(1).AddHours(2).AddMinutes(45));
+            Console.WriteLine(span);
+            Console.ReadKey();
+        }
+        public static string ComputeTime(DateTime dateTimeStart, DateTime dateTimeEnd)
+        {
+            TimeSpan span = (dateTimeEnd - dateTimeStart);
+            return String.Format($"{span.Days} days, {span.Hours} hours, {span.Minutes} minutes, {span.Seconds} seconds, {span.Milliseconds} milliseconds");
+        }
+    }
+}
